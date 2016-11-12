8452ac1b3738b364e75878dff280272525c21bff
Aleksandr Meshkov <agrobov@gmail.com>
2016-11-12 21:23:22 +0300

    Handle not importing data.table bug

--- R/am.checky.r
+++ R/am.checky.r
@@ -3,12 +3,14 @@
 #' Define pairwise correlations between the dependant variable (Y) and every independent variable (Xs). Be careful! Parallel computation in use.
 #'
 #' @param ds data.table: data set
+#' @param ds.list character vector: nams of independent variables (X)
 #' @param ds.y character: name of the dependent variable (Y)
 #' @param corr.type character: correlation types; "spearman" (default) and "pearson" available; see Hmisc rcorr for details
 #' @param cl.number number: cluster number for parallel computers; be very careful with this parameter! do not set it too big
 #' @param out.file character: absolute or relative path to the files with output; due to parallel computations you do NOT see most of the info in the console; id default no outputs are used
 #' @return Data.table. rn - feature name, cr - correlation value
 #' @seealso You can use this data to choose and create new features you prefer with am.calcf function
+#' @import data.table
 #' @export
 
 am.checky <- function(ds
@@ -27,22 +29,26 @@ am.checky <- function(ds
 
   registerDoParallel(cl)
 
+
   print(paste("start calculating correlations to Y :", length(ds.list), "steps", sep=" "))
 
   rtrn <- foreach (i=1:length(ds.list), .packages=c("Hmisc","data.table"), .verbose=FALSE, .combine=rbind) %dopar% {
-    tmp<-list(ds.list[i],rcorr(x = ds[,eval(as.name(ds.list[i]))], y = ds[,eval(as.name(ds.y))], type=eval(corr.type))$r[1,2])
+    tmp<-list(ds.list[i], rcorr(x = ds[, eval(as.name(ds.list[i]))], y = ds[, eval(as.name(ds.y))], type=eval(corr.type))$r[1,2])
   }
 
   print(paste("end calculating correlations to Y :", length(ds.list), "steps", sep=" "))
 
-  rtrn<-data.table(rtrn)
-  rtrn[,V2:=as.numeric(V2)]
+  rtrn <- data.table(rtrn)
+  rtrn[, V2 := as.numeric(V2)]
 
-  setnames(rtrn,c("V1","V2"),c("rn","cr"))
-  rtrn[,rn:=unlist(rn)]
+  setnames(rtrn, c("V1","V2"), c("rn","cr"))
+  rtrn[, rn := unlist(rn)]
 
   gc()
 
   stopCluster(cl)
   rtrn
 }
+
+.datatable.aware = TRUE
+
